c93441e4798a31e958112e4ed4bad69cbfb2257b
Alessandro Lima <alessandroslima@hotmail.com>
2023-09-26 22:05:22 -0700

    Links for pages according to the number of records on the list of contacts

--- js/script.js
+++ js/script.js
@@ -6,5 +6,32 @@ function updateCounter() {
     h3Element.innerHTML = `Total: ${numOfContacts}`;
 }
 
+/* Render numerical links for the pages of pagination */
+function makePagesSummary() {
+
+    const numOfPages = Math.ceil(contacts.length/10);
+    const ul = document.querySelector('ul.pagination');
+    for (let i=0; i<numOfPages; i++) {
+        const a = document.createElement('a');
+        a.setAttribute('href', '#');
+        a.setAttribute('data-page-number', `${i}`); // property data for page number
+        a.classList.add('pagination');
+        a.textContent = `${i+1}`;
+        a.addEventListener('click', pageNumberClick); // event handler for each link
+        const li = document.createElement('li');
+        li.classList.add('pagination');
+        li.textContent = "  ";
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+}
+
+/* Captures page number clicked and render the corresponding one */
+function pageNumberClick(event) {
+
+    event.preventDefault();
+}
+
 /* Section for setting up Events Triggering */
 document.addEventListener('DOMContentLoaded', updateCounter);
+document.addEventListener('DOMContentLoaded', makePagesSummary);
